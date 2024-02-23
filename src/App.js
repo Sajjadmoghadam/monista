6f973743879ef6d56d232f18f6a64853f31a1a11
Sajjadmoghadam <sajjad.moghadam1379@gmail.com>
2024-02-23 20:28:31 +0330

    create custom theme and component file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Index';
+import Footer from './Components/Footer/Index';
+import { createTheme, CssBaseline, ThemeProvider, Typography } from '@mui/material';
+
 
 function App() {
-  return (
+  const MoeinstaTheme = createTheme({
 
-    <>
+    palette: {
+      primary: { main: '#A274FF' },
+      secondary: { main: '#3D007C' },
+      white: {
+        main: '#F1F5F9 '
+      },
+      background: {
+        default: '#171717',
+      },
+      
+    },
+    typography:{
+      fontFamily: 'iransans , moraba'
+    }
+  }
+  
+  )
 
-    </>
-  );
+return (
+
+  <>
+    <ThemeProvider theme={MoeinstaTheme} >
+      <CssBaseline />
+      <Header />
+
+      <Typography sx={{ color: 'white.main',  }}> سلام</Typography>
+
+
+
+      <Footer />
+    </ThemeProvider>
+  </>
+);
 }
 
 export default App;
