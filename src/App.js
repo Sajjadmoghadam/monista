6d5f21cd4392c444ef64d991d134fbc4ee47e601
Sajjadmoghadam <sajjad.moghadam1379@gmail.com>
2024-02-24 18:56:23 +0330

    create section two
    create heading  and p tag
    and create service slider

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {prefixer} from 'stylis';
 import {CacheProvider} from '@emotion/react';
 import createCache from '@emotion/cache';
 import WestIcon from '@mui/icons-material/West';
+import OurServices from './Components/Swiper/OurServices';
 
 
 
@@ -45,18 +46,22 @@ return (
   <CacheProvider value={cacheRtl}>
     <ThemeProvider theme={MoeinstaTheme} >
       <CssBaseline />
-      <Box className='firstdiv'><Header />
+      <Box className='firstdiv'>
+      <Header />
+      <Stack className='mamad' mx={'auto'}  sx={{width:'1018px'}}>
       <Stack  direction={'column'} alignItems={'center'} justifyContent={'center'} sx={{height:'80vh' ,}}>
-
       <Typography textAlign={'center'} fontFamily={'moraba'} fontSize={'72px'} > بهترین ها را از مونیستا بخواهید!<br/> ما تفاوت ها را رقم میزیم </Typography>
       <Typography fontSize={'24px'} textAlign={'center'} marginBottom={'24px'} marginTop={'64px'} sx={{width:'825px'}}>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</Typography>
       <Button size='large' endIcon={<WestIcon/>} sx={{border:'2px solid #A274FF' ,borderRadius:'16px' , color:'white.main'}}> تماس با ما </Button>
-
-
       </Stack>
-
-
-
+      <Stack sx={{ textAlign:'center'}}>
+        <Typography variant='h2' fontFamily={'moraba'} fontSize={'42px'}>گروه ما چه خدماتی ارائه میدهد؟</Typography>
+        <Typography  textAlign={'center'} fontSize={'24px'} sx={{marginBottom:'57px', marginTop:'57px'}}>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</Typography> 
+      </Stack>
+      </Stack>
+      <Stack >
+      <OurServices/>
+      </Stack>
       </Box>
       <Footer />
     </ThemeProvider>
---
+++ src/Components/Swiper/OurServices/index.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Box, Stack, Typography } from '@mui/material';
+import './style.css'
+
+
+
+const OurServices = () => {
+    return (
+        <>
+         <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        
+        coverflowEffect={{
+          rotate: 50,
+          stretch:0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={false}
+        modules={[EffectCoverflow, Pagination]}
+        className="service-swiper-container"
+      >
+        <SwiperSlide className='service-swiper-item'>
+          <Stack p={'34px'} sx={{backdropFilter:'blur(8.7px)',  background:" linear-gradient(180deg, rgba(241, 245, 249, 0.20) 0%, rgba(241, 245, 249, 0.10) 100%)" , borderRadius:'24px' ,}} >
+            <Stack>
+              <Typography variant='h3' fontFamily={'moraba'} fontSize={'28px'} paddingBottom={'58px'}>طراحی سایت  فروشگاهی</Typography>
+            </Stack>
+            <Stack direction={'row'} gap={'28px'} alignItems={'start'} >
+              <Box><Typography className='mamad' fontSize={'12px'} sx={{textAlign:'justify' }}>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</Typography></Box>
+              <Box><img className='services-img' src="assets/image/web.png" alt="" /></Box>
+            </Stack>
+          </Stack>
+        </SwiperSlide>
+        <SwiperSlide className='service-swiper-item'>
+          <Stack p={'34px'} sx={{backdropFilter:'blur(8.7px)',  background:" linear-gradient(180deg, rgba(241, 245, 249, 0.20) 0%, rgba(241, 245, 249, 0.10) 100%)" , borderRadius:'24px' ,}} >
+            <Stack>
+              <Typography variant='h3' fontFamily={'moraba'} fontSize={'28px'} paddingBottom={'58px'}>طراحی سایت  فروشگاهی</Typography>
+            </Stack>
+            <Stack direction={'row'} gap={'28px'} alignItems={'start'} >
+              <Box><Typography className='mamad' fontSize={'12px'} sx={{textAlign:'justify' }}>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</Typography></Box>
+              <Box><img className='services-img' src="assets/image/web.png" alt="" /></Box>
+            </Stack>
+          </Stack>
+        </SwiperSlide>
+        <SwiperSlide className='service-swiper-item'>
+          <Stack p={'34px'} sx={{backdropFilter:'blur(8.7px)',  background:" linear-gradient(180deg, rgba(241, 245, 249, 0.20) 0%, rgba(241, 245, 249, 0.10) 100%)" , borderRadius:'24px' ,}} >
+            <Stack>
+              <Typography variant='h3' fontFamily={'moraba'} fontSize={'28px'} paddingBottom={'58px'}>طراحی سایت  فروشگاهی</Typography>
+            </Stack>
+            <Stack direction={'row'} gap={'28px'} alignItems={'start'} >
+              <Box><Typography className='mamad' fontSize={'12px'} sx={{textAlign:'justify' }}>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</Typography></Box>
+              <Box><img className='services-img' src="assets/image/web.png" alt="" /></Box>
+            </Stack>
+          </Stack>
+        </SwiperSlide>
+      
+        
+      </Swiper>
+        
+        
+        
+        </>
+    );
+}
+
+export default OurServices;
